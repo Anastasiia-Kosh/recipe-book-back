5187bb77fe135e05f3365c0657a8a2478d69b899
Anastasiia Koshutska <zaporozhetsnastya89@gmail.com>
2026-08-19 12:55:33 +0300

    Initial commit

---
+++ src/controllers/authController.js
@@ -0,0 +1,170 @@
+import createHttpError from 'http-errors';
+import { User } from '../models/user.js';
+import bcrypt from 'bcrypt';
+import { Session } from '../models/session.js';
+import { createSession, setSessionCookies } from '../services/auth.js';
+import { sendEmail } from '../utils/sendMail.js';
+import jwt from 'jsonwebtoken';
+import handlebars from 'handlebars';
+import path from 'node:path';
+import fs from 'node:fs/promises';
+
+export const registerUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  const existingUser = await User.findOne({ email });
+  if (existingUser) {
+    throw createHttpError(400, 'Email in use');
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const newUser = await User.create({
+    email,
+    password: hashedPassword,
+  });
+
+  const newSession = await createSession(newUser._id);
+  setSessionCookies(res, newSession);
+  res.status(201).json(newUser);
+};
+
+export const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  // Перевіряємо чи користувач з такою поштою існує
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw createHttpError(401, 'Invalid credentials');
+  }
+
+  // Порівнюємо хеші паролів
+  const isValidPassword = await bcrypt.compare(password, user.password);
+  if (!isValidPassword) {
+    throw createHttpError(401, 'Invalid credentials');
+  }
+  await Session.deleteOne({ userId: user._id });
+  const newSession = await createSession(user._id);
+  setSessionCookies(res, newSession);
+  res.status(200).json(user);
+};
+
+export const logoutUser = async (req, res) => {
+  const { sessionId } = req.cookies;
+
+  if (sessionId) {
+    await Session.deleteOne({ _id: sessionId });
+  }
+
+  res.clearCookie('sessionId');
+  res.clearCookie('accessToken');
+  res.clearCookie('refreshToken');
+
+  res.status(204).send();
+};
+
+export const refreshUserSession = async (req, res) => {
+  const { sessionId, refreshToken } = req.cookies;
+
+  if (!sessionId || !refreshToken) {
+    throw createHttpError(401, 'Missing session credentials');
+  }
+
+  // 1. Знаходимо поточну сесію за id сесії та рефреш токеном
+  const session = await Session.findOne({
+    _id: sessionId,
+    refreshToken,
+  });
+
+  // 2. Якщо такої сесії нема, повертаємо помилку
+  if (!session) {
+    throw createHttpError(401, 'Session not found');
+  }
+
+  // 3. Якщо сесія існує, перевіряємо валідність рефреш токена
+  const isSessionTokenExpired = session.refreshTokenValidUntil < new Date();
+
+  // Якщо термін дії рефреш токена вийшов,
+  // видаляємо сесію і повертаємо помилку
+  if (isSessionTokenExpired) {
+    await session.deleteOne();
+    res.clearCookie('sessionId');
+    res.clearCookie('accessToken');
+    res.clearCookie('refreshToken');
+    throw createHttpError(401, 'Session token expired');
+  }
+
+  // 4. Якщо всі перевірки пройшли добре, видаляємо поточну сесію
+  await session.deleteOne();
+
+  // 5. Створюємо нову сесію та додаємо кукі
+  const newSession = await createSession(session.userId);
+  setSessionCookies(res, newSession);
+
+  res.status(200).json({
+    message: 'Session refreshed',
+  });
+};
+
+export const requestResetEmail = async (req, res) => {
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) {
+    return res
+      .status(200)
+      .json({ message: 'Password reset email sent successfully' });
+  }
+
+  const resetToken = jwt.sign(
+    { sub: user._id, email: req.body.email },
+    process.env.JWT_SECRET,
+    { expiresIn: '15m' },
+  );
+
+  // 1. Формуємо шлях до шаблона
+  const templatePath = path.resolve('src/templates/reset-password-email.html');
+  // 2. Читаємо шаблон
+  const templateSource = await fs.readFile(templatePath, 'utf-8');
+  // 3. Готуємо шаблон до заповнення
+  const template = handlebars.compile(templateSource);
+  // 4. Формуємо із шаблона HTML документ з динамічними даними
+  const html = template({
+    name: user.username,
+    link: `${process.env.FRONTEND_DOMAIN}/reset-password?token=${resetToken}`,
+  });
+
+  try {
+    await sendEmail({
+      from: process.env.SMTP_FROM,
+      to: req.body.email,
+      subject: 'Reset your password',
+      html,
+    });
+  } catch {
+    throw createHttpError(
+      500,
+      'Failed to send the email, please try again later.',
+    );
+  }
+
+  res.status(200).json({});
+};
+
+export const resetPassword = async (req, res) => {
+  const { password, token } = req.body;
+
+  let payload;
+  try {
+    payload = jwt.verify(token, process.env.JWT_SECRET);
+  } catch {
+    throw createHttpError(401, 'Invalid or expired token');
+  }
+
+  const user = await User.findOne({ _id: payload.sub, email: payload.email });
+  if (!user) {
+    throw createHttpError(404, 'User not found');
+  }
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  await User.updateOne({ _id: user._id }, { password: hashedPassword });
+  await Session.deleteMany({ userId: user._id });
+  res.status(200).json({ message: 'Password reset successfully' });
+};
---
+++ src/server.js
@@ -0,0 +1,36 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import { connectMongoDB } from './db/connectMongoDB.js';
+import { errorHandler } from './middleware/errorHandler.js';
+import { notFoundHandler } from './middleware/notFoundHandler.js';
+import { logger } from './middleware/logger.js';
+import notesRoutes from './routes/notesRoutes.js';
+import authRoutes from './routes/authRoutes.js';
+import userRoutes from './routes/userRoutes.js';
+import { errors } from 'celebrate';
+import cookieParser from 'cookie-parser';
+
+const app = express();
+const PORT = process.env.PORT ?? 3000;
+
+app.use(express.json());
+app.use(cors());
+app.use(logger);
+app.use(cookieParser());
+
+app.use(authRoutes);
+app.use(notesRoutes);
+app.use(userRoutes);
+// обробка 404
+app.use(notFoundHandler);
+// обробка помилок від celebrate (валідація)
+app.use(errors());
+// глобальна обробка інших помилок
+app.use(errorHandler);
+
+await connectMongoDB();
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
