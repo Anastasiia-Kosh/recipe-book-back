515102bd00ac1d480dae663e414ce6969419dcd7
Анастасія Кошуцька <zaporozhetsnastya89@gmail.com>
2026-09-04 17:19:25 +0300

    update pino

--- src/middleware/logger.js
+++ src/middleware/logger.js
@@ -1,16 +1,23 @@
 import pino from 'pino-http';
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 export const logger = pino({
   level: 'info',
-  transport: {
-    target: 'pino-pretty',
-    options: {
-      colorize: true,
-      translateTime: 'HH:MM:ss',
-      ignore: 'pid,hostname',
-      messageFormat:
-        '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
-      hideObject: true,
-    },
-  },
+
+  ...(isProduction
+    ? {}
+    : {
+        transport: {
+          target: 'pino-pretty',
+          options: {
+            colorize: true,
+            translateTime: 'HH:MM:ss',
+            ignore: 'pid,hostname',
+            messageFormat:
+              '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
+            hideObject: true,
+          },
+        },
+      }),
 });
